15ec85cab954bd459d6d6ca1d1db472aafa549d9
im25a-gonnetb <im25a-gonnetb@users.noreply.github.com>
2026-05-12 09:29:57 +0200

    added code to initiate db

--- backend.py
+++ backend.py
@@ -1,5 +1,15 @@
 #this is a python file
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+#load db
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 
 # Create FastAPI app instance
 app = FastAPI()
